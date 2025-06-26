dbad7975be2f354e8deaf4973f0ad98cb4aa0e70
Hailey Burgess <hailey.burgess@redcross.org>
2025-06-25 22:08:37 -0400

    database seeded

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,17 @@
--- TODO
+
+DROP TABLE IF EXISTS folders CASCADE;
+DROP TABLE IF EXISTS files;
+
+
+CREATE TABLE files (
+   id serial PRIMARY KEY,
+    name text NOT NULL
+);
+
+CREATE TABLE folders (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    size int NOT NULL,
+    folder_id int NOT NULL REFERENCES files(id) ON DELETE CASCADE,
+    UNIQUE (name, folder_id)
+);
--- db/seed.js
+++ db/seed.js
@@ -6,5 +6,26 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  const foldersList = [
+    { name: "folder1" },
+    { name: "folder2" },
+    { name: "folder3" },
+  ];
+  const filesList = [
+    { name: "file1", size: 3, folder_id: 1 },
+    { name: "file2", size: 2, folder_id: 1 },
+    { name: "file3", size: 1, folder_id: 1 },
+    { name: "file4", size: 2, folder_id: 1 },
+    { name: "file5", size: 1, folder_id: 1 },
+    { name: "file6", size: 5, folder_id: 2 },
+    { name: "file7", size: 5, folder_id: 2 },
+    { name: "file8", size: 5, folder_id: 2 },
+    { name: "file9", size: 5, folder_id: 2 },
+    { name: "file10", size: 5, folder_id: 2 },
+    { name: "file11", size: 5, folder_id: 3 },
+    { name: "file12", size: 5, folder_id: 3 },
+    { name: "file13", size: 5, folder_id: 3 },
+    { name: "file14", size: 5, folder_id: 3 },
+    { name: "file15", size: 5, folder_id: 3 },
+  ];
 }
